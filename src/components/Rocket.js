9b340f57b0865f7acc3be591609570a4c73d1b50
Maha Magdy <mahamagdy3@gmail.com>
2021-10-13 12:41:39 +0200

    Implement rocket booking - Actions, and update the rocketsReducer based on this

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,15 +1,33 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable react/prop-types, no-unused-vars */
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from '../redux/rockets/rockets';
+
 export default function Rocket(props) {
+  const dispatch = useDispatch();
   const { rocket } = props;
+  const reserveThisRocket = () => {
+    dispatch(reserveRocket(rocket.id));
+  };
+
   return (
     <div className="d-flex">
       <div>
-        <img src={rocket.flickr_images[0]} alt={rocket.name} className="figure-img" />
+        <img
+          src={rocket.flickr_images[0]}
+          alt={rocket.name}
+          className="figure-img"
+        />
       </div>
       <div className="ps-3">
         <h3>{rocket.name}</h3>
         <p>{rocket.description}</p>
-        <button type="button" className="btn btn-primary">Reserve Rocket</button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={reserveThisRocket}
+        >
+          Reserve Rocket
+        </button>
       </div>
     </div>
   );
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,13 +1,23 @@
+/* eslint-disable consistent-return */
 // import axios from 'axios';
 
 const ROCKETS_LIST = 'space_travelers_hub/rockets/ROCKETS_LIST';
+const RESERVE_ROCKET = 'space_travelers_hub/rockets/RESERVE_ROCKET';
 
 const initialState = [];
 export default function rocketsReducer(state = initialState, action) {
+  let newState = [];
   switch (action.type) {
     case ROCKETS_LIST:
-      console.log('Come to your Mummy', action.payload);
       return [...state, ...action.payload];
+    case RESERVE_ROCKET:
+      newState = state.map((rocket) => {
+        if (rocket.id === action.payload) {
+          return { ...rocket, reserved: true };
+        }
+        return rocket;
+      });
+      return [...newState];
     default:
       return state;
   }
@@ -15,5 +25,10 @@ export default function rocketsReducer(state = initialState, action) {
 
 export const rocketsList = (payload) => ({
   type: ROCKETS_LIST,
+  payload,
+});
+
+export const reserveRocket = (payload) => ({
+  type: RESERVE_ROCKET,
   payload,
 });
